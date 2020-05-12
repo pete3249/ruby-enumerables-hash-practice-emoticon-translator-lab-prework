8558bab54f87718de0af26c8c4a2039b09a7072b
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-12 16:19:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,6 +14,10 @@ end
 def get_english_meaning(emoticons_dictionary, emoticon)
   loaded_emoticons = load_library(emoticons_dictionary) 
   binding.pry
+  loaded_emoticons.each do |key, values|
+    if loaded_emoticons[:get_meaning] = emoticon
+      english_meaning = loaded_emoticons[:get_meaning][emoticon]
+      puts english_meaning
 end
 
 
