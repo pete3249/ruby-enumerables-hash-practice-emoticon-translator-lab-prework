804393acd8be73c7ccc9e1b2f7f22b58e3ac650a
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-12 16:05:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ def load_library(emoticons_dictionary)
   loaded_emoticons_dictionary.each do |name, symbols|
     binding.pry
     sorted_dictionary[:get_meaning][symbols[1]] = name
-    sorted_dictionary[:get_emoticon][symbols[0]] = loaded_emoticons_dictionary[name][1]
+    sorted_dictionary[:get_emoticon][symbols[0]] = symbols[1]
   end
   sorted_dictionary
 end
