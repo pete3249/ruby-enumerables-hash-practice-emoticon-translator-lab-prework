5c89b9dc3eb6ae8b620dccca744eacbdbdb82c6a
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-12 14:56:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,18 +2,6 @@ require 'yaml'
 emoticons_dictionary = YAML.load_file('lib/emoticons.yml')
 
 def load_library(emoticons_dictionary)
-  sorted_dictionary = {}
-  emoticons_dictionary.each do |name, symbols|
-    symbols.each do |symbol|
-      if sorted_dictionary[name]
-        sorted_dictionary[name][:japanese] << symbol.to_s
-      else
-        sorted_dictionary[name] = {:english => "", :japanese => ""}
-        sorted_dictionary[name][:english] << symbol.to_s
-      end
-    end
-  end
-  sorted_dictionary
 end
 
 def get_japanese_emoticon
